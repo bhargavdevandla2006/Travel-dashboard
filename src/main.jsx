85ab5ce4db155849fc6708030ddfee68ef9dfec4
Bhargav Devandla <bhargavdevandla2006@gmail.com>
2026-08-05 16:46:17 +0530

    Connected ThemeProvider to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {MapProvider} from  "./context/MapContext"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MapProvider } from "./context/MapContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
-createRoot(document.getElementById('root')).render(
-    <MapProvider>
-    <App />
-  </MapProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider>
+      <MapProvider>
+        <App />
+      </MapProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
